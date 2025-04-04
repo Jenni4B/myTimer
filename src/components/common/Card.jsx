b2b2c25d194242a0d1149b59efd1927beafd62df
Jenni4B <jennifer.ea.blocker@gmail.com>
2025-04-04 09:15:53 -0400

    Changed the padding on card

--- src/components/common/Card.jsx
+++ src/components/common/Card.jsx
@@ -7,16 +7,16 @@ const Card = () => {
   // Get initialSeconds from context instead of trying to access the ref
   
   return (
-    <section className="bg-gray-900 text-white p-6 rounded-lg shadow-lg w-96 flex flex-col items-center" style={{
+    <div className="bg-gray-900 text-white p-6 rounded-lg shadow-lg w-96 flex flex-col items-center" style={{
       height: "160px", 
-      border: "2px solidrgba(255, 255, 255, 0.7)", // White border
-      boxShadow: "0 0 5px rgba(255,255,255,0.5)", // Glow effect
-      padding: "20px"
+      border: "3px solid rgba(255, 255, 255, 0.7)", // White border
+      boxShadow: "0 0 5px rgba(255, 255, 255, 0.8)", // Glow effect
+      padding: "40px"
     }}>
       <ErrorBoundary>
         <TimerControls />
       </ErrorBoundary>
-    </section>
+    </div>
   );
 };
 
